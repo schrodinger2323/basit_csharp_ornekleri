5e76863d5b00b87b29fed17482c4b31277774344
schrodinger2323 <152845558+schrodinger2323@users.noreply.github.com>
2024-03-16 23:01:25 +0300

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//klavyeden girilen stringteki kelime sayisini bulan pr.
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir seyler yaziniz");
+            string str;
+            str= Console.ReadLine();
+            int sayac = 1;
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (str[i] ==' ' )
+                {
+                    sayac++;
+                }
+
+            }
+            Console.WriteLine(sayac + " kelime vardir");
+            //kelime_sayi_bul();
+        }
+        static void kelime_sayi_bul()
+        {
+            Console.WriteLine("string giriniz");
+            string s= Console.ReadLine();
+            string[] words= s.Split(' ');
+            Console.WriteLine("kelime sayisi= " + words.Length);
+
+
+        }
+    }
+}
