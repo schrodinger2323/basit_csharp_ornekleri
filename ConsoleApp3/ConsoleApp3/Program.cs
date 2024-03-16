e1bf8bbbb96963ea30fab1c16598ac71d3dcd599
schrodinger2323 <152845558+schrodinger2323@users.noreply.github.com>
2024-03-16 23:00:12 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//bolme sonucu kalani bulma
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float bolunen, bolen, kalan, sonuc;
+            Console.WriteLine("bolunen sayiyi girin ");
+            bolunen=Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("bolen sayiyi girin ");
+            bolen=Convert.ToSingle(Console.ReadLine());
+            kalan = bolunen % bolen;
+            sonuc = bolunen / bolen;
+            Console.WriteLine("kalan= " + kalan);
+            Console.WriteLine("sonuc= " + sonuc);
+           
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//klavyeden girilen sayinin basamak sayisini bulma
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir tam sayi giriniz");
+            int num, sayac = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num == 0) { Console.WriteLine(1); }
+            else
+            {
+                if (num < 0) { num = -num; }
+                while (num > 0)
+                {
+                    sayac++;
+                    num = num / 10;
+
+                }
+                Console.WriteLine(sayac);
+            }
+        }
+       }
+ }
+
