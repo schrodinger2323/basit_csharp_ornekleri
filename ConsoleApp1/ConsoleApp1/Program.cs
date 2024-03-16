254566c21db89f192ca77fadaabce06a2f0dac80
schrodinger2323 <152845558+schrodinger2323@users.noreply.github.com>
2024-03-16 22:58:04 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//klavyeden girilen iki sayiyi carpan program
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float number1, number2, product;
+            Console.WriteLine("1. sayi: ");
+            number1=Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("2. sayi: ");
+            number2= Convert.ToSingle(Console.ReadLine());
+            product=number1*number2;
+            Console.WriteLine(number1 + " * " + number2 + " = " + product);
+
+        }
+    }
+}
