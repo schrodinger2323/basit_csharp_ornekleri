9231e8da70284303fb12faef96e054f175faeb05
schrodinger2323 <152845558+schrodinger2323@users.noreply.github.com>
2024-03-16 23:03:35 +0300

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//a^b hesaplayan pr.
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double us, taban;
+            Console.WriteLine("sayi tabani giriniz");
+            taban=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("us giriniz");
+            us = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Pow(taban,us));
+            
+
+        }
+    }
+}
